0d462ac8e49851af846674bd4b13efd10378e973
Muerl <haagmm@gmail.com>
2013-02-23 02:28:55 -0500

    cleaned up build

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-del HardData.zip
-7z a -tzip HardData.zip -r *.* -x!.* -x!*-* -x!bin -x!src -x!HDF_IOS_ANDROID -x!fuckwit -x!*~
+del HardDataFH.zip
+7z a -tzip HardDataFH.zip -r *.* -x!.* -x!*-* -x!bin -x!src -x!HDF_IOS_ANDROID -x!fuckwit -x!*~ -x!copy
 
-..\..\net.rim.browser.tools.wcpc_1.5.1.201010291444-22\wcpc\bbwp.exe HardData.zip /g quarkquark
+..\..\net.rim.browser.tools.wcpc_1.5.1.201010291444-22\wcpc\bbwp.exe HardDataFH.zip /g quarkquark
 
-copy /Y bin\StandardInstall\HardData.cod "C:\Program Files\Research In Motion\BlackBerry Smartphone Simulators 6.0.0\6.0.0.294 (9780)"
+copy /Y bin\StandardInstall\HardDataFH.cod "C:\Program Files\Research In Motion\BlackBerry Smartphone Simulators 6.0.0\6.0.0.294 (9780)"
